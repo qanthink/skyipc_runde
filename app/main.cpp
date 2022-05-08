a81760e08eb748d5a7434460acf4d77befb8749b
qanthink <qanthink@163.com>
2022-05-09 00:37:53 +0800

    Update ffmpeg lib. Resample pcm file.

--- app/main.cpp
+++ app/main.cpp
@@ -9,19 +9,50 @@ qanthink 版权所有。
 
 #include "ao.hpp"
 #include "audio_player.h"
+#include "mp3decoder.h"
+
+extern "C" {
+#include <libavutil/opt.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswresample/swresample.h>
+}
+
 
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
+	int volume = -25;
+	const char *filePath = NULL;
+
+	#if 0
+	if(3 != argc)
+	{
+		cerr << "Please useage ./skyipc volume ./xxx.wav" << endl;
+		cerr << "For example: /customer/skyipc -30 /customer/xiaopingguo.wav" << endl;
+		cerr << "For example: /customer/skyipc 5 /mnt/linux/Audios/wav/16K_16bit_MONO_30s.wav" << endl;
+		cerr << "Support by qanthink@163.com" << endl;
+		return -1;
+	}
+	else
+	{
+		filePath = argv[2];
+		volume = atoi(argv[1]);
+	}
+	#endif
+
 	AudioOut *pAudioOut = AudioOut::getInstance();			// init Mi_AUDIO_OUT
-	pAudioOut->setVolume(-30);
+	pAudioOut->setVolume(volume);
 	AudioPlayer *pAudioPlayer = AudioPlayer::getInstance();	// init audio player.
 
+	#if 1
 	// test PCM
-	//pAudioPlayer->playPCM("/mnt/linux/Music/pcm_16000_16bit.pcm");
-	//sleep(5);
+	//pAudioPlayer->playPCM("/mnt/linux/Audios/pcm/pcm_16000_16bit.pcm");
+	pAudioPlayer->playPCM("/mnt/linux/Audios/pcm/xiaopingguo_mono_16b_16000.pcm");
+	#endif
 
+	#if 0
 	// test WAV
 	pAudioPlayer->playWAV("/mnt/linux/Audios/my_wav/1234567890abcde.wav");
 	sleep(5);		// remove sleep() in your codes. There is just a sample.
@@ -37,19 +68,27 @@ int main(int argc, const char *argv[])
 	sleep(10);
 	pAudioPlayer->playWAV("/mnt/linux/Audio/my_wav/1234567890abcde.wav");
 	sleep(10);
+	#endif
+	
+	#if 0
+	pAudioPlayer->playWAV(filePath);
+	//sleep(5);		// remove sleep() in your codes. There is just a sample.
+	#endif
+
+	#if 0
+	Mp3Decoder *pMp3Decoder = Mp3Decoder::getInstance();
+	//pMp3Decoder->test();
+	pMp3Decoder->pcmFileResample("/mnt/linux/Audios/pcm/xiaopingguo_mono_16b_16000.pcm", 16000, AV_CH_LAYOUT_MONO, AV_SAMPLE_FMT_S16, \
+				"/mnt/linux/Audios/pcm/xiaopingguo_stero_32b_44100.pcm", 44100, AV_CH_LAYOUT_STEREO, AV_SAMPLE_FMT_FLT);
+	#endif
 
 	while(true)
 	{
 		sleep(1);
 		static int sleepCntSec = 0;
-		if(0 == (++sleepCntSec % 10))
+		if(4 == (++sleepCntSec))
 		{
-			cout << "Progress running." << endl;
-		}
-		
-		if(60 * 1 == sleepCntSec)		// n * 60 = n min.
-		{
-			break;
+			return 0;
 		}
 	}
 	
